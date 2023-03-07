54a99390d597fb4ce392e09d55963ae57c986f88
Canoura <sthefanyacostac@gmail.com>
2023-03-07 18:27:37 +0000

    creamos nuestro metodo para conectarnos con la api de marvel

---
+++ PruebaTecnicaAngular/pruebamarvel/src/app/personajes/personaje/compartir/personaje-api.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonajeApiService {
+PUBLIC_KEY = '17556370aa05dfb6038d0542e78dcb0e';
+HASH = '0f7f6340c25ebaf1db4ba95911e3777c';
+URL_API ='https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=17556370aa05dfb6038d0542e78dcb0e&hash=0f7f6340c25ebaf1db4ba95911e3777c';
+
+  constructor(private http: HttpClient) { }
+
+  getAllPersonajes(): Observable<any> {
+    return this.http.get<any>(this.URL_API)
+    .pipe(map((data: any)=> data))
+  }
+}
